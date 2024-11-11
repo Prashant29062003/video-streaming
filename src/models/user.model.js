31bfd9af36f7cd92c3ad670f5a9345ec317e10cf
Prashant Kumar <prashantkumar6425@gmail.com>
2024-11-11 23:34:11 +0530

    some debugging

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -39,7 +39,7 @@ const userSchema = new Schema({
     watchHistory: [
         {
             type: Schema.Types.ObjectId,
-            ref: Video
+            ref: "Video"
         }
     ],
     refreshToken: {
